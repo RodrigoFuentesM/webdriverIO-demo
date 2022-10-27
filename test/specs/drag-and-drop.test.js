f7ffa426b78628ae4528c25a881dfe47c667152e
Rodrigo Fuentes <rodrigo.fuentes@globant.com>
2022-10-27 18:04:39 -0300

    test[Added example of dragAndDrop for jqueryui page]

--- test/specs/drag-and-drop.test.js
+++ test/specs/drag-and-drop.test.js
@@ -1,6 +1,6 @@
 const dragAndDrop = require('../pageobjects/drag-and-drop.page');
 
-describe('Drag and Drop page test suite', () => {
+xdescribe('Drag and Drop page test suite', () => {
 
   beforeEach(async() => {
     await dragAndDrop.open();
@@ -14,4 +14,19 @@ describe('Drag and Drop page test suite', () => {
   });
 
 
+});
+
+describe('Multiple Windows test suite', () => {
+  before(async() => {
+    await  browser.url("https://jqueryui.com/resources/demos/droppable/default.html")
+  });
+  it('should switch to the next window', async() => {
+      const elem = await $('#column-a')
+      const target = await $('#column-b')
+      //await  browser.pause(5000)
+      // drag and drop to other element
+      await elem.dragAndDrop(target);
+      await expect(target).toHaveText('Dropped!');
+
+  })
 });
